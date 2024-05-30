7b285dc3011a5ea63da228d4d7f07a9ebd80c91e
Max Revitt <max@revitt.consulting>
2024-05-30 12:41:56 +0100

    feat(docker): dockerize

---
+++ run_benchmarks.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+PROFILE=$1
+RPC_URL=$2
+PRIVATE_KEY=$3
+
+cat <<EOF > ./profiles.json
+{
+  "profiles": {
+    "$PROFILE": {
+      "node_url": "$RPC_URL",
+      "key_file": "$PRIVATE_KEY"
+    }
+  }
+}
+EOF
+
+export PROFILE=$PROFILE
+
+
+## debug remove
+cat ./profiles.json
+
+docker compose build
+docker compose up --abort-on-container-exit --exit-code-from benchmark
+EXIT_CODE=$?
+docker compose down
+
+echo "Benchmark exit code for profile ${PROFILE}: $EXIT_CODE"
+exit $EXIT_CODE
